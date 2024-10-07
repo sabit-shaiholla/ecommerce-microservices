7379353a7808e6cbf0dbfe39b67d3740c27dfc77
sabit-shaiholla <sabit.shaiholla@nu.edu.kz>
2024-10-07 23:40:03 +0500

    Added Payment service with PostgreSQL database

---
+++ services/payment/src/main/java/com/electroboy/ecommerce/payment/PaymentRepository.java
@@ -0,0 +1,6 @@
+package com.electroboy.ecommerce.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+}
---
+++ services/payment/src/main/java/com/electroboy/ecommerce/payment/PaymentService.java
@@ -0,0 +1,32 @@
+package com.electroboy.ecommerce.payment;
+
+import com.electroboy.ecommerce.notification.NotificationProducer;
+import com.electroboy.ecommerce.notification.PaymentNotificationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+    private final NotificationProducer notificationProducer;
+
+    public Integer createPayment(PaymentRequest request) {
+        var payment = paymentRepository.save(paymentMapper.toPayment(request));
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstname(),
+                        request.customer().lastname(),
+                        request.customer().email()
+                )
+        );
+
+        return payment.getId();
+    }
+}
