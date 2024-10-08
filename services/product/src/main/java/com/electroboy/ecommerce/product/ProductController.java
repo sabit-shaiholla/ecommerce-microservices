7162ef93ccabe81a2474d2136fdaa6985c9342f9
sabit-shaiholla <sabit.shaiholla@nu.edu.kz>
2024-10-09 00:40:00 +0500

    Added Swagger config and documentation for Product Service API

--- services/product/src/main/java/com/electroboy/ecommerce/product/ProductController.java
+++ services/product/src/main/java/com/electroboy/ecommerce/product/ProductController.java
@@ -1,5 +1,8 @@
 package com.electroboy.ecommerce.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "Product Service API", description = "Product management API")
 public class ProductController {
 
     private final ProductService productService;
 
     @PostMapping
+    @Operation(summary = "Create a new product", description = "Create a new product with the provided details")
+    @ApiResponse(responseCode = "200", description = "Product created successfully")
     public ResponseEntity<Integer> createProduct(
             @RequestBody @Valid ProductRequest request
     ) {
@@ -22,6 +28,8 @@ public class ProductController {
     }
 
     @PostMapping("/purchase")
+    @Operation(summary = "Purchase products", description = "Purchase products with the provided details")
+    @ApiResponse(responseCode = "200", description = "Products purchased successfully")
     public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(
             @RequestBody @Valid List<ProductPurchaseRequest> request
     ) {
@@ -29,6 +37,8 @@ public class ProductController {
     }
 
     @GetMapping("/{product-id}")
+    @Operation(summary = "Find product by ID", description = "Find a product by the provided ID")
+    @ApiResponse(responseCode = "200", description = "Product retrieved successfully")
     public ResponseEntity<ProductResponse> findProductById(
             @PathVariable("product-id") Integer productId
     ) {
@@ -36,6 +46,8 @@ public class ProductController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all products", description = "Retrieves the list of all products")
+    @ApiResponse(responseCode = "200", description = "List of products retrieved successfully")
     public ResponseEntity<List<ProductResponse>> findAllProducts() {
         return ResponseEntity.ok(productService.findAllProducts());
     }
