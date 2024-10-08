6dd791975162a7828bd7f253cb193c6122003b30
sabit-shaiholla <sabit.shaiholla@nu.edu.kz>
2024-10-09 00:46:08 +0500

    Added Swagger config and documentation for Payment Service API

--- services/payment/src/main/java/com/electroboy/ecommerce/payment/PaymentController.java
+++ services/payment/src/main/java/com/electroboy/ecommerce/payment/PaymentController.java
@@ -1,5 +1,8 @@
 package com.electroboy.ecommerce.payment;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/payments")
+@Tag(name = "Payment Service API", description = "Internal Payment management API")
 public class PaymentController {
 
     private final PaymentService paymentService;
 
     @PostMapping
+    @Operation(summary = "Create a new payment", description = "Create a new payment with the provided details")
+    @ApiResponse(responseCode = "200", description = "Payment created successfully")
     public ResponseEntity<Integer> createPayment(
             @RequestBody @Valid PaymentRequest request
     ) {
